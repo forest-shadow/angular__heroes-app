ab0115ff95d304cb376c6b04c50a8e24a5b7676c
forest-shadow <mamasay@yandex.ru>
2017-06-26 16:49:54 +0300

    add filter hero capability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -23,7 +24,13 @@ import { InMemoryDataService }  from './in-memory-data.service';
     InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
-  declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
   providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
